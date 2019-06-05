802aa1843e2967a959140b0a28a23d0b9a074b9e
alpden550 <alpden550@gmail.com>
2019-06-05 17:16:43 +0300

    Changed logic of bot's creating

--- insta_advertising.py
+++ insta_advertising.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 from instabot import Bot
 
 
-def get_post_id(url):
+def get_post_id(bot, url):
     return bot.get_media_id_from_link(url)
 
 
-def get_all_comments_for_post(post_id):
+def get_all_comments_for_post(bot, post_id):
     return [(comment['user_id'],
              comment['user']['username'],
              comment['text']) for comment in bot.get_media_comments_all(post_id)]
@@ -19,11 +19,10 @@ def find_names(text):
     # regular expression was taken https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
     pattern = re.compile(
         r'(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)')
-    result = pattern.findall(text)
-    return result
+    return pattern.findall(text)
 
 
-def is_any_user_exist(users):
+def is_any_user_exist(bot, users):
     return any(bot.get_user_id_from_username(user) for user in users)
 
 
@@ -35,8 +34,12 @@ def create_parser():
 
 
 def get_contestants(post_url, author):
-    post_id = get_post_id(post_url)
-    all_comments = get_all_comments_for_post(post_id)
+    load_dotenv()
+    bot = Bot()
+    bot.login(username=os.getenv('INST_LOGIN'), password=os.getenv('INST_PASSWORD'))
+
+    post_id = get_post_id(bot, post_url)
+    all_comments = get_all_comments_for_post(bot, post_id)
     contestants = []
     likers = bot.get_media_likers(post_id)
     followers = bot.get_user_followers(author)
@@ -44,8 +47,8 @@ def get_contestants(post_url, author):
     for comment in all_comments:
         inst_id, inst_author, inst_text = comment
         users_in_text = find_names(inst_text)
-        if (users_in_text and is_any_user_exist(users_in_text) and
-                str(inst_id) in likers and str(inst_id)) in followers:
+        if (users_in_text and is_any_user_exist(bot, users_in_text) and
+                str(inst_id) in likers and str(inst_id) in followers):
             contestants.append(inst_author)
 
     return set(contestants)
@@ -53,10 +56,7 @@ def get_contestants(post_url, author):
 
 if __name__ == "__main__":
     args = create_parser()
-    load_dotenv()
-    bot = Bot()
-    bot.login(username=os.getenv('INST_LOGIN'), password=os.getenv('INST_PASSWORD'))
 
-    print("We're counting contestants, please, wait. It can take some time...")
+    print("We're counting contestants, please, wait. It might take some time...")
     contestants = get_contestants(args.inst_post, args.inst_account)
     print(contestants)
